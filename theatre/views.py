3cf2273f7b3e8664e13a207c4f42712f35fccd10
Illya Maznitskiy <comercaleuros@gmail.com>
2024-08-07 13:24:05 +0300

    Add views in theatre app

--- theatre/views.py
+++ theatre/views.py
@@ -1,3 +1,63 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from theatre.models import (
+    TheatreHall,
+    Play,
+    Performance,
+    Actor,
+    Genre,
+    Reservation,
+    Ticket
+)
+from theatre.serializers import (
+    TheatreHallSerializer,
+    PlaySerializer,
+    PerformanceSerializer,
+    ActorSerializer,
+    GenreSerializer,
+    ReservationSerializer,
+    TicketSerializer
+)
+
+
+class TheatreHallViewSet(viewsets.ModelViewSet):
+    queryset = TheatreHall.objects.all()
+    serializer_class = TheatreHallSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class PlayViewSet(viewsets.ModelViewSet):
+    queryset = Play.objects.all()
+    serializer_class = PlaySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class PerformanceViewSet(viewsets.ModelViewSet):
+    queryset = Performance.objects.all()
+    serializer_class = PerformanceSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+    permission_classes = [permissions.IsAuthenticated]
