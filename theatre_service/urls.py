67a3d2200bab31f06af3edd7683c4d0e68cb953e
Illya Maznitskiy <comercaleuros@gmail.com>
2024-08-29 16:03:07 +0300

    Use prefix api for theatre and user pages

--- theatre_service/urls.py
+++ theatre_service/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("theatre/", include("theatre.urls", namespace="theatre")),
-    path("user/", include("user.urls", namespace="user")),
+    path("api/theatre/", include("theatre.urls", namespace="theatre")),
+    path("api/user/", include("user.urls", namespace="user")),
 ]
