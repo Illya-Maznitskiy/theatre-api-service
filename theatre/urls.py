25687666fdebd3483d4e058350c4f614722a71d1
Illya Maznitskiy <comercaleuros@gmail.com>
2024-09-05 12:11:12 +0300

    Use plural names in theatre/urls.py

--- theatre/urls.py
+++ theatre/urls.py
@@ -16,12 +16,6 @@ router = routers.SimpleRouter()
 router.register(
     "theatre-halls", TheatreHallViewSet, basename="theatre_halls"
 )
-router.register("plays", PlayViewSet)
-router.register("performances", PerformanceViewSet)
-router.register("actors", ActorViewSet)
-router.register("genres", GenreViewSet)
-router.register("reservations", ReservationViewSet)
-router.register("tickets", TicketViewSet)
 
 urlpatterns = [path("", include(router.urls))]
 
