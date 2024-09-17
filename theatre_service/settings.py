33d24865068dbf4a7e3a8a2c23190ec6419ae563
Illya Maznitskiy <comercaleuros@gmail.com>
2024-09-17 20:00:04 +0300

    Add correct DB setting and AUTH_USER_MODEL in settings.py

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,17 +93,24 @@ WSGI_APPLICATION = "theatre_service.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# Override settings if in Docker environment
+POSTGRES_HOST = "localhost"
+
+load_dotenv()
+
+if os.getenv("DOCKER_ENV") == "true":
+    POSTGRES_HOST = "db"
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_DB"),
-        "USER": os.environ.get("POSTGRES_USER"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-        "HOST": os.environ.get("POSTGRES_HOST"),
-        "PORT": os.environ.get("POSTGRES_PORT"),
+        "NAME": os.getenv("POSTGRES_DB", "theatre"),
+        "USER": os.getenv("POSTGRES_USER", "theatre"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "theatre"),
+        "HOST": POSTGRES_HOST,
+        "PORT": os.getenv("POSTGRES_PORT", "5435"),
     }
 }
-AUTH_USER_MODEL = "user.User"
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -143,6 +152,8 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
+
+AUTH_USER_MODEL = "user.User"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
